d018e311d7b2af1870a711894dc984f0fbcb29db
chrisbrickey <cbrickey1@gmail.com>
2017-04-20 17:43:35 -0700

    defined method that converts parsed HTML to array and strips p-tags

--- primary_file.rb
+++ primary_file.rb
@@ -2,32 +2,45 @@
 require 'nokogiri'
 require 'open-uri'
 
-scraped_object = Nokogiri::HTML(open("http://www.emilyslist.org/pages/entry/events"))
-# puts "scraped_object: #{scraped_object}"
+#Parameter requirements:
+#url_string must be in string format e.g. "http://www.testing123.com"
+#tag_pattern must also be in string format e.g. "//outertag//innertag"
+def scrape (url_string, tag_pattern)
 
-#below code pushes everything in between p-tags (that are also in between article tags) into separate elements of the NodeSet
-parsed_node_set = scraped_object.css("//article//p")
-# puts "parsed_node_set: #{parsed_node_set}"
+  #scrapes entire HTML document and places it in a Nokogiri object
+  # scraped_object = Nokogiri::HTML(open("http://www.emilyslist.org/pages/entry/events"))
+  scraped_object = Nokogiri::HTML(open(url_string))
+  # puts "scraped_object: #{scraped_object}"
 
-puts parsed_node_set[0].to_s
-puts "\n"
-puts parsed_node_set[1].to_s
-puts "\n"
-puts parsed_node_set[2].to_s
-puts "\n"
-puts parsed_node_set[3].to_s
+  #pushes everything contained in p-tags (if p-tag also contained within article-tag) into separate elements of the NodeSet (enumerable)
+      #in the case of Emily's List, p-tags only exist within an article-tag so the //article below is unnecessary
+  # parsed_node_set = scraped_object.css("//article//p")
+  parsed_node_set = scraped_object.css(tag_pattern)
+
+  parsed_array = []
+
+  #converts nodeset to array of strings
+  parsed_node_set.each do |element|
+    parsed_array << element.to_s
+  end
+
+  # strips the p-tags; may not be necessary
+  #!!!This needs to changed based on the length of the tag
+  parsed_array.map! do |str|
+    str[3..-5]
+  end
+
+  parsed_array
+end#of scrape method
 
 
-#target tag: <article id="content" class="base main-content" role="main">
+#Idea: Setup a hash to map URLs to parsing input so can loop through multiple URLs
+#e.g. url_map = {"http://www.emilyslist.org/pages/entry/events" => "//article//p"}
+#See this page for how to assign specific tags based on the URL: http://www.nokogiri.org/tutorials/searching_a_xml_html_document.html
+#doc.css('//car:tire', 'car' => 'http://alicesautoparts.com/')
 
-
-
-
-
-
-
-
-
+puts scrape("http://www.emilyslist.org/pages/entry/events", "//article//p")
+#Emily's list full target tag: <article id="content" class="base main-content" role="main">
 
 
 
