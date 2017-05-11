2f4af116b4632a9a7eb4d6d3238b21082e4f33c9
chrisbrickey <cbrickey1@gmail.com>
2017-05-10 17:48:45 -0700

    updated after Emilyslist changed website

--- emilys_event_urls_to_csv.rb
+++ emilys_event_urls_to_csv.rb
@@ -1,4 +1,5 @@
-#I used this program to help confirm the similarity of individual event urls for Emilys list
+#I used this program to help confirm the similarity of individual event urls for Emilys list,
+#...but in early May, the website setup changed and this no longer pulls event data
 
 require 'nokogiri'
 require 'open-uri'
@@ -6,7 +7,7 @@ require 'csv'
 
 
 # EMILYS LIST EVENT 1
-data_from_event_url1 = Nokogiri::HTML(open("http://www.emilyslist.org/2017"))  #only 1 upcoming event
+data_from_event_url1 = Nokogiri::HTML(open("https://secure.emilyslist.org/Chicago_YP17"))  #only 1 upcoming event
 parsed_from_url1 = data_from_event_url1.css("section")
 
 CSV.open('raw_data/event1_raw.csv', 'w') do |csv|
@@ -15,7 +16,7 @@ end
 
 
 # EMILYS LIST EVENT 2 (confirmed that html is in same format as event 1...upcoming events)
-data_from_event_url2 = Nokogiri::HTML(open("http://www.flickr.com/photos/emilyslist/sets/72157679761839052"))  #all other events are in the past and stored at flickr
+data_from_event_url2 = Nokogiri::HTML(open("https://www.flickr.com/photos/emilyslist/albums/72157680519659093"))  #all other events are in the past and stored at flickr
 parsed_from_url2 = data_from_event_url1.css("section")
 
 
