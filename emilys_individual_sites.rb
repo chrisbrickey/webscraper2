d3371f10560206a8ac5e1f264068e6a599759697
chrisbrickey <cbrickey1@gmail.com>
2017-05-10 17:22:16 -0700

    established structure for combining data from all urls

--- emilys_individual_sites.rb
+++ emilys_individual_sites.rb
@@ -1,13 +1,43 @@
-require 'nokogiri' #also required by ScrapeEventURLs module
-require 'open-uri' #also required by ScrapeEventURLs module
-require 'json'
-require 'csv'
-require "./scrape_event_urls.rb"
+require 'nokogiri' #required by ScrapeEventURLs module
+require 'open-uri' #required by ScrapeEventURLs module
+require 'json'     #required by CreateJsonObject module
+
+require './scrape_event_urls'
 include ScrapeEventURLs
 
+require './create_json_object'
+include CreateJsonObject
 
-#create array of event URLs from Emily's list:
-main_URL = "http://www.emilyslist.org/pages/entry/events"
-event_urls_tag_pattern = "//article//p//a/@href"
-emilys_url_array = ScrapeEventURLs.create(main_URL, event_urls_tag_pattern)[1..-1] #0th element is NOT an event
-puts emilys_url_array
+
+
+def pull_emily_individual_event_data(event_url)
+  "placeholder"
+end
+
+
+def combine_all_events(url_array)
+  events_array = []
+  url_array.each { |event_url| events_array << pull_emily_individual_event_data(event_url) }
+  events_array
+end
+
+
+
+
+
+emilys_main_URL = "http://www.emilyslist.org/pages/entry/events"
+emilys_event_urls_tag_pattern = "//article//p//a/@href"
+
+emilys_url_array = ScrapeEventURLs.create(emilys_main_URL, emilys_event_urls_tag_pattern)[1..-1] #0th element is NOT an event on Emily's list
+# puts emilys_url_array           #array of strings
+# puts emilys_url_array.length
+
+emilys_final_object_all_urls = combine_all_events(emilys_url_array)
+puts emilys_final_object_all_urls
+puts emilys_final_object_all_urls.length #make sure same as url_array.length
+
+
+
+#NEXT STEPS:
+#See this page for how to assign specific tags based on the URL: http://www.nokogiri.org/tutorials/searching_a_xml_html_document.html
+#doc.css('//car:tire', 'car' => 'http://alicesautoparts.com/')
