016e83080f4095bffd41dba536d1f2c66ff1d4fa
chrisbrickey <cbrickey1@gmail.com>
2017-05-19 15:47:35 -0700

    setup complete, pulls event title, just needs logic for remaining variables

--- emilys_individual_sites.rb
+++ emilys_individual_sites.rb
@@ -9,32 +9,54 @@ require './create_json_object'
 include CreateJsonObject
 
 
-#event_url must be a string
-def pull_emily_individual_event_data(event_url)
-  "placeholder"
-  # ScrapeEventURLs.create(event_url, "html") #not working, Emilys list appears to have changed setup
+#STEP 1: CREATES THE ARRAY OF EVENT-SPECIFIC URLS
+def pull_event_urls(main_url, event_url_tag_pattern)
+  ScrapeEventURLs.create(main_url, event_url_tag_pattern)
+end
+
+emilys_main_URL = "http://www.emilyslist.org/pages/entry/events"
+emilys_event_url_tag_pattern = "//article//p//a/@href"
+emilys_url_array = pull_event_urls(emilys_main_URL, emilys_event_url_tag_pattern)[1..-1] #0th element is NOT an event on Emily's list
+# puts emilys_url_array
+
+
+
+#STEP 2: PARSE THE DATA FOR A SINGLE URL AND CREATE JSON OBJECT
+def pull_emilys_event_data(event_website) #event_website must be a string
+
+  event_title = ScrapeEventURLs.create(event_website, ".bsd-contribForm-aboveContent/h1")[0][7..-8]
+  description = "not yet pulled" #can't be an empty string for CTA aggregator
+  start_time = "not yet pulled"
+  end_time = "not yet pulled"
+  event_location = "not yet pulled"
+
+  #These categories can't yet be pulled from the url data, so they are given assumptions based on knowledge of Emilys List events in general
+  free = false
+  cta_type = "onsite"
+
+  CreateJsonObject.create_json_object(event_title, description, free, start_time, end_time, cta_type, event_website, event_location)
+
 end
 
 
+#TESTING TAG PATTERNS...the block we want is within "<div class="bsd-contribForm-aboveContent">
+# testing_tag_pattern = "div#main-content.full-bleed-bg" #pulls too much, couldn't narrow with h1 or p
+# testing_tag_pattern = ".bsd-contribForm-aboveContent/p" #WORKS FOR DATE/TIME/LOCATION
+
+testing_url = emilys_url_array[0] #only upcoming event on main url
+puts pull_emilys_event_data(testing_url)
+
+
+
+#STEP 3: PUSHES ALL JSON EVENT OBJECTS (ONE FROM EACH URL) INTO AN ARRAY PER JSON API SPEC
 def combine_all_events(url_array)
   events_array = []
   url_array.each { |event_url| events_array << pull_emily_individual_event_data(event_url) }
   events_array
 end
 
-
-
-
-
-emilys_main_URL = "http://www.emilyslist.org/pages/entry/events"
-emilys_event_urls_tag_pattern = "//article//p//a/@href"
-
-emilys_url_array = ScrapeEventURLs.create(emilys_main_URL, emilys_event_urls_tag_pattern)[1..-1] #0th element is NOT an event on Emily's list
-puts emilys_url_array           #array of strings
-# puts emilys_url_array.length
-
-emilys_final_object_all_urls = combine_all_events(emilys_url_array)
-puts emilys_final_object_all_urls
+# emilys_final_object_all_urls = combine_all_events(emilys_url_array)
+# puts emilys_final_object_all_urls
 # puts emilys_final_object_all_urls.length #make sure same as url_array.length
 
 
