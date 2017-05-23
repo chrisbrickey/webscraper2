c9a6a8c4ef485c540a89edb9c01a4775ffdb15ef
chrisbrickey <cbrickey1@gmail.com>
2017-05-23 10:17:34 -0700

    removed event_location from object because should not be present for initial cta creation

--- create_json_object.rb
+++ create_json_object.rb
@@ -2,7 +2,7 @@ require 'json'
 
 module CreateJsonObject
 
-  def create_json_object(event_title, description, free, start_time, end_time, cta_type, event_website, event_location)
+  def create_json_object(event_title, description, free, start_time, end_time, cta_type, event_website)
 
     json_event_object = {
          "data": {
@@ -16,9 +16,10 @@ module CreateJsonObject
                "cta-type": cta_type,          #String ("onsite" or "phone")
                "website": event_website       #String
              },
+             #leaving location blank because needs to be added after original CTA is created
              "relationships": {
                "location": {
-                 "data": { "type": "locations", "id": event_location } #String
+                 "data": { "type": "locations", "id": "" }
                },
                "contact": {
                  "data": { "type": "contacts", "id": "" }
@@ -32,8 +33,6 @@ module CreateJsonObject
     }
 
     json_event_object.to_json #.to_json is required to get this object to be accepted by CTA aggregator
-    # json_event_object
-    #NEXT STEP: make sure date maintains integer format when converted to json object!
   end
 
 end#of module
