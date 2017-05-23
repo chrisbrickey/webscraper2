5514219653020f6c582e2d4d20cc7d649c5e56ec
chrisbrickey <cbrickey1@gmail.com>
2017-05-23 02:54:20 -0700

    start_time and end_time formatted as long strings including date but without use of module

--- emilys_individual_sites.rb
+++ emilys_individual_sites.rb
@@ -1,12 +1,16 @@
 require 'nokogiri' #required by ScrapeEventURLs module
 require 'open-uri' #required by ScrapeEventURLs module
 require 'json'     #required by CreateJsonObject module
+require 'date'     #required by CreateDateTimeObject
 
 require './scrape_event_urls'
 include ScrapeEventURLs
 
 require './create_json_object'
 include CreateJsonObject
+
+# require './create_date_time_object'
+# include CreateDateTimeObject
 
 
 #STEP 1: CREATES THE ARRAY OF EVENT-SPECIFIC URLS
@@ -28,34 +32,38 @@ def pull_emilys_event_data(event_website) #event_website must be a string
 
   description = "not yet pulled" #can't be an empty string for CTA aggregator
 
-  raw_date_times, raw_location = ScrapeEventURLs.create(event_website, ".bsd-contribForm-aboveContent/p")[1].split("<br><br>")
+  date_times_raw, location_raw = ScrapeEventURLs.create(event_website, ".bsd-contribForm-aboveContent/p")[1].split("<br><br>")
 
-  event_location = raw_location[2..-9].gsub("\r\n", "").gsub("<br>", ", ")
-  # puts "event_location: #{event_location}"
+  event_location = location_raw[2..-9].gsub("\r\n", "").gsub("<br>", ", ")
+
+  stripped_date_times_raw = date_times_raw.gsub("\n", "").gsub("\r", "").gsub("<p>", "")
+  date_raw, both_times = stripped_date_times_raw.split("<br>")
+  start_time_raw, end_time_raw = both_times.split(" - ")
 
 
-  stripped_raw_date_times = raw_date_times.gsub("\n", "").gsub("\r", "").gsub("<p>", "")
-  date, both_times = stripped_raw_date_times.split("<br>")
+  # start_time, end_time = CreateDateTimeObject.create()
 
-  start_time, end_time = both_times.split(" - ")
+  date_string = Date.parse(date_raw)
 
-  #determines a.m. or p.m. for start_time
+  #determines a.m. or p.m. for start_time if isn't already noted
   meridiem = String.new
-  unless start_time.chars.any? { |l| ["A", "a", "P", "p"].include?(l) }
-    if end_time.chars.any? { |l| ["A", "a"].include?(l) }
+  unless start_time_raw.chars.any? { |l| ["A", "a", "P", "p"].include?(l) }
+    if end_time_raw.chars.any? { |l| ["A", "a"].include?(l) }
       meridiem = "a.m."
     else
       meridiem = "p.m."
     end
 
-    start_time += " #{meridiem}"
+    start_time_raw += " #{meridiem}"
   end
 
+  start_time_24hr = DateTime.parse(start_time_raw).strftime("%H:%M")
+  end_time_24hr = DateTime.parse(end_time_raw).strftime("%H:%M")
 
-  start_time = "#{date} #{start_time}"
-  puts "start_time: #{start_time}\n\n"
-  end_time = "#{date} #{end_time}"
-  puts "end_time: #{end_time}\n\n"
+  start_time = "#{date_string} #{start_time_24hr} +0000"
+  end_time = "#{date_string} #{end_time_24hr} +0000"
+
+  # event_date = Date.parse event_date   #transforms string to date integer
 
 
   #These categories can't yet be pulled from the url data, so they are given assumptions based on knowledge of Emilys List events in general
