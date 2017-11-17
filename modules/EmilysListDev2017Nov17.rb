32a9287b19351f049424a61bc9459ca8a5de4ee6
chrisbrickey <cbrickey1@gmail.com>
2017-11-17 11:48:45 -0800

    copy emilyslist scraper from current cta-aggregator dev branch

---
+++ modules/EmilysListDev2017Nov17.rb
@@ -0,0 +1,40 @@
+require 'nokogiri'
+require 'open-uri'
+
+class EmilysList
+  def events
+    raw_page = open("http://www.emilyslist.org/pages/entry/events").read
+    page = Nokogiri::HTML(raw_page)
+    events = []
+    page.xpath('//h2[text()="Upcoming Events"]/following-sibling::p').each do |p|
+      event = Hash.new
+
+      date_and_location = p.css('strong').first
+
+      next unless date_and_location
+
+      date_loc_parts = date_and_location.text.split("\n")
+      event['start_date'] = Date.parse(date_loc_parts[0])
+      loc_parts = date_loc_parts[1].split(', ')
+      event['location'] = {
+        'locality': loc_parts[0],
+        'region': loc_parts[1]
+      }
+
+      links = p.css('a')
+      next unless links.first
+      event['title'] = links.first.content
+      event_url = p.xpath('./a[1]/@href').first.content
+      event['browser_url'] = event_url
+      event['identifiers'] = ["emilyslist:#{event_url.split('/').last}"]
+
+      event['origin_system'] = "Emily's List"
+
+      # past events link to flickr sets, not event pages
+      if /secure\.emilyslist\.org/ =~ event_url
+        events << event
+      end
+    end
+    events
+  end
+end
